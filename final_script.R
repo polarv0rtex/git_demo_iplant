d1d35bbcc99c80add94b39c0e68c6ab10b21880b
Brendan Mcdermott <bmcdermott@vm65-217.iplantcollaborative.org>
2015-02-22 16:36:32 -0700

    working through the final script

---
+++ final_script.R
@@ -0,0 +1,13 @@
+# Working Script
+# Brendan McDermott
+# Feb 22, 2015
+
+install.packages("ggplot2", dependencies = TRUE)
+install.packages("plyr")
+install.packages("ggthemes")
+install.packages("reshape2")
+
+library("ggplot2")
+library("reshape2")
+library("ggthemes")
+library("plyr")
