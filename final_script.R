cb87eb6267a335682d3df58ee170ea2fefc7ff23
Brendan Mcdermott <bmcdermott@vm65-217.iplantcollaborative.org>
2015-02-22 16:59:13 -0700

    Working on the final charts for the day

--- final_script.R
+++ final_script.R
@@ -11,3 +11,21 @@ library("ggplot2")
 library("reshape2")
 library("ggthemes")
 library("plyr")
+
+# set working directory
+setwd("~/git_demo_iplant")
+
+# import the tsv
+data.in <- read.delim("output/combined_gapMinder.tsv")
+
+# extract 3 countries: Cameroon, Venezuela, and Yemen
+countries <- subset(data.in, 
+                      country == "Cameroon" |
+                      country == "Venezuela" |
+                      country == "Zambia")
+
+# plot points
+summary(countries)
+
+ggplot(countries, aes(year, pop, color=country)) + 
+  geom_point(size = 5) 
